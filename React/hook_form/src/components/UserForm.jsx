7591424d5fd7afccc8e948c17dbe1be742b1b3eb
RyanHeishman <heishy0@gmail.com>
2023-07-11 09:05:50 -0700

    Hook Form Assignment

---
+++ React/hook_form/src/components/UserForm.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const UserForm = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  
+    
+    const createUser = (e) => {
+        // we must prevent the default refresh of the browser to keep our state from being reset
+        e.preventDefault();
+    
+        // create a javascript object to hold all of the values
+        const newUser = { 
+            firstName, 
+            lastName,
+            email, 
+            password 
+        };
+        console.log("Welcome", newUser);
+
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPassword("");
+    };
+    
+    return(
+        <form onSubmit={ createUser }>
+            <div>
+                <label>First Name: </label> 
+                <input type="text" onChange={ (e) => setFirstName(e.target.value) } value={firstName}/>
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" onChange={ (e) => setLastName(e.target.value) } value={lastName}/>
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } value={email}/>
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="text" onChange={ (e) => setPassword(e.target.value) } value={password}/>
+            </div>
+            <input type="submit" value="Create User" />
+
+            <h3>Your Form Data</h3>
+            <p>First Name: {firstName}</p>
+            <p>Last Name: {lastName}</p>
+            <p>Email: {email}</p>
+            <p>Password: {password}</p>
+        </form>
+
+
+
+    );
+};
+    
+export default UserForm;
