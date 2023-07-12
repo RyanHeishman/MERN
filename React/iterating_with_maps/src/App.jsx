079873f4c9e46227be3f9f614095e8d9c1777c83
RyanHeishman <heishy0@gmail.com>
2023-07-12 09:18:42 -0700

    map iteration practice

---
+++ React/iterating_with_maps/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Groceries from './components/Groceries'
+
+function App() {
+
+  return (
+    <>
+      <Groceries/>
+    </>
+  )
+}
+
+export default App
